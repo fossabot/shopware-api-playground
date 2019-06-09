a659de148dbd71c0f11c0c599827e3c896276fad
Gruselhaus <nico@gruselhaus.com>
2019-06-09 14:00:06 +0200

    Removing shopware/ and adding shopware/ to .gitignore

--- shopware/dev-ops/common/actions/check-code.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-#DESCRIPTION: Check the code for php-cs-fixer and php-stan issues
-
-sh dev-ops/common/actions/.cs-fixer.sh "__PHP_VERSION__"
-sh dev-ops/common/actions/.phpstan.sh "__PHP_VERSION__"
--- shopware/dev-ops/common/actions/init.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-#DESCRIPTION: initialization of your environment
-
-cp /usr/local/bin/composer composer.phar
-ant -f build/build.xml build-unit
